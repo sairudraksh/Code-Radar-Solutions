d6f37e5205ff7378fe3f3bb9e4dbbdceb51d8a4e
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-03-28 15:17:17 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,14 +1,18 @@
-trachPlayer(int ranked[n],int n,int player[m],int result[m]){
+void trachPlayer(int ranked[],int n,int player[],int resul[]){
     int x=1;
+    int k=0;
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(ranked[j]==n-1 && ranked[j]<player[i]){
                 printf("%d\n",x+1);
+                arr[k]=x;
+                k++;
                 x=1;
                 break;
             }
             else if(ranked[j]<player[i]){
-                printf("%d\n",x);
+                arr[k]=x;
+                k++;
                 x=1;
                 break;
             }
