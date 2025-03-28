bff1a357ce6829cd971425fb84e4b5cabeab078b
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-03-28 15:11:19 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,31 +1,18 @@
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int n1;
-    scanf("%d",&n1);
-    int arr2[n1];
-    for(int i=0;i<n1;i++){
-        scanf("%d",&arr2[i]);
-    }
+trachPlayer(int ranked[n],int n,int player[m],int result[m]){
     int x=1;
-    for(int i=0;i<n1;i++){
+    for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            if(arr[j]==n-1 && arr[j]<arr2[i]){
+            if(ranked[j]==n-1 && ranked[j]<player[i]){
                 printf("%d\n",x+1);
                 x=1;
                 break;
             }
-            else if(arr[j]<arr2[i]){
+            else if(ranked[j]<player[i]){
                 printf("%d\n",x);
                 x=1;
                 break;
             }
-            else if(arr[j]>arr2[i]){
+            else if(ranked[j]>player[i]){
                 x++;
             }
         }
